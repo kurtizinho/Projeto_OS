83cb6dfadac5fb94449742ba0d5d5c0910a9bf66
Edygleyson Lima <edygleyson2010@hotmail.com>
2022-09-24 20:38:09 -0300

    ok

--- install.py
+++ install.py
@@ -1,5 +1,29 @@
 #/bin/python3
 
 import os
+from getpass import getpass
 
-os.system('/opt/eunix/Projeto_OS/')
+_host = input('Endereço do Banco: ')
+_string = input('String de conexão: ')
+_user = getpass('User: ')
+_pass = getpass('Pass: ')
+
+with open('/opt/Projeto_OS/software/ora.conf', 'w+') as file:
+    file.write(f"""host={_host}
+string={_string}
+user={_user}
+pass={_pass}""")
+
+
+def exec(_command):
+    os.system(_command)
+
+
+commands = ['install unzip',
+            '/opt/Projeto_OS/ora_install.py',
+            'cp /opt/Projeto_OS/eunix_os.service /etc/systemd/system/'
+            'chmod -R 770 /opt/Projeto_OS',
+            'systemctl enbale eunix_os && systemctl start eunix_os']
+
+for x in commands:
+    exec(x)
