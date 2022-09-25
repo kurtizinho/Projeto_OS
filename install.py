a60fa1b803c957ece949dbdb9116aa987ce7ea35
Edygleyson Lima <edygleyson2010@hotmail.com>
2022-09-24 22:18:00 -0300

    ok

--- install.py
+++ install.py
@@ -2,13 +2,18 @@
 
 import os
 from getpass import getpass
+try:
+    from tqdm import tqdm
+except:
+    os.system('pip3 install tqdm')
+    from tqdm import tqdm
 
 _host = input('Endereço do Banco: ')
 _string = input('String de conexão: ')
 _user = getpass('User: ')
 _pass = getpass('Pass: ')
 
-with open('/opt/Projeto_OS/software/ora.conf', 'w+') as file:
+with open('ora.conf', 'w+') as file:
     file.write(f"""host={_host}
 string={_string}
 user={_user}
@@ -25,5 +30,5 @@ commands = ['apt install unzip',
             'chmod -R 770 /opt/Projeto_OS',
             'systemctl enable eunix_os && systemctl start eunix_os']
 
-for x in commands:
+for x in tqdm(commands):
     exec(x)
